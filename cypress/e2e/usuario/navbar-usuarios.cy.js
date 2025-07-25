24d15a6d18ad8ab986fb79371608a29bd4cd8219
Shere Cis <cis.shere@gmail.com>
2025-07-25 16:04:31 -0300

    Realice la visulación del navbar en pc y mobile de los 3 usuarios y realice un formateo automatico a mi codigo

---
+++ cypress/e2e/usuario/navbar-usuarios.cy.js
@@ -0,0 +1,73 @@
+describe("Página inicial de ticketazo", () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.visit("https://vps-3696213-x.dattaweb.com/");
+  });
+
+  it("Navbar del admin desde mi pc", () => {
+    cy.fixture("usuarios").then((users) => {
+      cy.login(users.usuarioAdmin.email, users.usuarioAdmin.password);
+    });
+    cy.viewport(1366, 768);
+    cy.fixture("elementosNav").then((elementos) => {
+      cy.itemsNavPc(elementos.navAdmin);
+    });
+  });
+
+  it("Navbar del admin desde mi simulador de mobile", () => {
+    cy.fixture("usuarios").then((users) => {
+      cy.login(users.usuarioAdmin.email, users.usuarioAdmin.password);
+    });
+    cy.viewport("iphone-8");
+    cy.fixture("elementosNav").then((elementos) => {
+      cy.itemsNavMobile(elementos.navAdmin);
+    });
+  });
+
+  it("Navbar del comprador desde mi pc", () => {
+    cy.fixture("usuarios").then((users) => {
+      cy.login(users.usuarioComprador.email, users.usuarioComprador.password);
+    });
+    cy.viewport(1366, 768);
+    cy.fixture("elementosNav").then((elementos) => {
+      cy.itemsNavPc(elementos.navComprador);
+    });
+  });
+
+  it("Navbar del comprador desde mi simulador de mobile", () => {
+    cy.fixture("usuarios").then((users) => {
+      cy.login(users.usuarioComprador.email, users.usuarioComprador.password);
+    });
+    cy.viewport("iphone-8");
+    cy.fixture("elementosNav").then((elementos) => {
+      cy.itemsNavMobile(elementos.navComprador);
+    });
+  });
+
+  it("Navbar del organizador desde mi pc", () => {
+    cy.fixture("usuarios").then((users) => {
+      cy.login(
+        users.usuarioOrganizador.email,
+        users.usuarioOrganizador.password
+      );
+    });
+    cy.viewport(1366, 768);
+    cy.fixture("elementosNav").then((elementos) => {
+      cy.itemsNavPc(elementos.navOrganizador);
+    });
+  });
+
+  it("Navbar del Organizador desde mi simulador de mobile", () => {
+    cy.fixture("usuarios").then((users) => {
+      cy.login(
+        users.usuarioOrganizador.email,
+        users.usuarioOrganizador.password
+      );
+    });
+    cy.viewport("iphone-8");
+    cy.fixture("elementosNav").then((elementos) => {
+      cy.itemsNavMobile(elementos.navOrganizador);
+    });
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,19 @@
-Cypress.Commands.add('login', (email, password) => {
-  cy.get('.justify-end .text-sm').click() // boton de login
-  cy.get('[data-cy="input-email"]').type(email)
-  cy.get('[data-cy="input-password"]').type(password)
-  cy.get('[data-cy="btn-login"]').click()
+Cypress.Commands.add("login", (email, password) => {
+  cy.get(".justify-end .text-sm").click(); // boton de login
+  cy.get('[data-cy="input-email"]').type(email);
+  cy.get('[data-cy="input-password"]').type(password);
+  cy.get('[data-cy="btn-login"]').click();
+});
+
+Cypress.Commands.add("itemsNavMobile", (elementos) => {
+  cy.get(".lg\\:hidden > .justify-end > .z-0").click();
+  elementos.forEach((elemento) => {
+    cy.get(".pt-2 li").contains(elemento);
+  });
+});
+
+Cypress.Commands.add("itemsNavPc", (elementos) => {
+  elementos.forEach((elemento) => {
+    cy.get("nav").first().contains(elemento);
+  });
 });
