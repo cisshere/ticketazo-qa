bb7cb388a2e52b0bb942b6d84c9a129ceb91cf81
Shere Cis <cis.shere@gmail.com>
2025-07-28 14:33:35 -0300

    modifique navbarUsuarios

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,9 +11,42 @@ Cypress.Commands.add("itemsNavMobile", (elementos) => {
     cy.get(".pt-2 li").contains(elemento);
   });
 });
+/*********************************************************** */
+//instalar -> npm install --save-dev cypress-file-upload
+import "cypress-file-upload"; //lib externa p/ simulacion se suba de img
 
-Cypress.Commands.add("itemsNavPc", (elementos) => {
-  elementos.forEach((elemento) => {
-    cy.get("nav").first().contains(elemento);
+
+Cypress.Commands.add("mensajeToast", (textoEsp) => {
+  cy.get(".z-50 > .flex-grow", { timeout: 10000 }).should(($mensaje) => {
+    //me aseguro que esté visible
+    expect($mensaje).to.have.css("opacity", "1");
+    expect($mensaje.text().trim()).to.include(textoEsp);
   });
 });
+
+Cypress.Commands.add("itemsNavPc", (elementos) => {
+  cy.get("nav")
+    .find("li")
+    .then((items) => {
+      const elementosEnPantalla = [...items].map((el) => el.innerText.trim());
+      expect(elementosEnPantalla).to.deep.equal(elementos);
+    });
+});
+
+Cypress.Commands.add("itemsNavMobile", (elementos) => {
+  cy.get(".lg\\:hidden > .justify-end > .z-0").click();
+  cy.get(".pt-2 li").then((items) => {
+    const elementosEnPantalla = [...items].map((el) => el.innerText.trim());
+    expect(elementosEnPantalla).to.deep.equal(elementos);
+  });
+});
+
+Cypress.Commands.add("loginCliente", () => {
+  cy.get(":nth-child(1) > .relative > .inline-flex").type(
+    "eventos@eventoselek.com"
+  );
+  cy.get('[data-cy="input-password"]').type("Eventos18.");
+  cy.get('[data-cy="btn-login"]').click();
+});
+
+import "cypress-file-upload";
