b4be5dca1b6deef5b189823f12c2f1cfde970b67
Elias <eliasperrota1999.j@gmail.com>
2025-07-29 13:44:45 -0300

    Modifique la carpeta de carga y un test

--- cypress/e2e/registro/carga-pagina-registro-organizador.cy.js
+++
@@ -1,8 +0,0 @@
-// Este es un caso de prueba que se encarga de verificar que la pagina de registro de organizador se carga correctamente
-describe('Carga de la página de registro organizador', () => {
-    it('Debería cargar la página de registro de organizador correctamente', () => {
-    cy.log('Verificando que la página de registro de organizador se carga correctamente');
-    cy.visit('https://ticketazo.com.ar/auth/registerClient')   
-
-    });
-});
